32afe2bb61b532d99eb1925b195abccab62e5b64
mrcarembole <carembole@gmail.com>
2019-07-11 16:36:28 +0200

    .

--- hosts/vm-example/configuration.sh
+++ hosts/vm-example/configuration.sh
@@ -12,7 +12,7 @@ zfs_pool_name="rpool"
 zfs_pool_type=""           # use "" for single, or "mirror", "raidz1", etc.
 
 # Note: using /dev/disk/by-id is also preferable.
-zfs_pool_disks=("/dev/sda")
+zfs_pool_disks=("/dev/vda" "/dev/vdb")
 
 # Datasets to be set with com.sun:auto-snapshot=true.
 zfs_auto_snapshot=("$zfs_pool_name/HOME" "$zfs_pool_name/ROOT")
